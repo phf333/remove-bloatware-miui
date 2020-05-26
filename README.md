0c0d499db479f660dc0c807d1a0f595142cf2a1f
Paulo Henrique <phf333@gmail.com>
2020-05-26 19:14:43 -0300

    update bashscript

---
+++ README.md
@@ -0,0 +1,19 @@
+#default list of apps 
+
+
+
+Google Photos
+Google Duo
+Facebook shits
+Youtube
+Play Movies
+
+Mi Browser
+Mi Feedback
+Mi Compass
+Mi Video
+Mi Notes
+Mi Music
+Mi Drop
+Mi Apps
+Miui system advertising
--- removebloatwaremiui.sh
+++ removebloatwaremiui.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-echo "Uninstalling apps..."
+echo "Do you want to remove bloatware of xiaomi?(use y or n): "
+read resp;
+if [ $resp == "y" ];
+    then
+
+    echo "Uninstalling apps..."
 
 
-adb shell pm uninstall -k --user 0 com.google.android.apps.photos # Google Photos
-adb shell pm uninstall -k --user 0 com.android.browser # Mi Browser
-adb shell pm uninstall -k --user 0 com.miui.bugreport # Mi Feedback
-adb shell pm uninstall -k --user 0 com.miui.compass # Mi Compass
-adb shell pm uninstall -k --user 0 com.miui.notes # Mi Notes
-adb shell pm uninstall -k --user 0 com.miui.videoplayer # Mi Video
-adb shell pm uninstall -k --user 0 com.miui.player # Mi Music
-adb shell pm uninstall -k --user 0 com.xiaomi.midrop # Mi Drop
-adb shell pm uninstall -k --user 0 com.xiaomi.mipicks # Mi Apps
-adb shell pm uninstall -k --user 0 com.xiaomi.scanner # Mi Scanner
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon # Google Duo
-adb shell pm uninstall -k --user 0 com.google.android.videos # Play Movies
+
+    adb shell pm uninstall -k --user 0 com.google.android.apps.photos # Google Photos
+    adb shell pm uninstall -k --user 0 com.android.browser # Mi Browser
+    adb shell pm uninstall -k --user 0 com.miui.bugreport # Mi Feedback
+    adb shell pm uninstall -k --user 0 com.miui.compass # Mi Compass
+    adb shell pm uninstall -k --user 0 com.miui.notes # Mi Notes
+    adb shell pm uninstall -k --user 0 com.miui.videoplayer # Mi Video
+    adb shell pm uninstall -k --user 0 com.miui.player # Mi Music
+    adb shell pm uninstall -k --user 0 com.xiaomi.midrop # Mi Drop
+    adb shell pm uninstall -k --user 0 com.xiaomi.mipicks # Mi Apps
+    adb shell pm uninstall -k --user 0 com.xiaomi.scanner # Mi Scanner
+    adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon # Google Duo
+    adb shell pm uninstall -k --user 0 com.google.android.videos # Play Movies
 
 
-adb shell pm uninstall -k --user 0 com.android.mms #Mensajes
-adb shell pm uninstall -k --user 0 com.miui.calculator #Calculadora
-adb shell pm uninstall -k --user 0 com.android.calendar #Calendario
-adb shell pm uninstall -k --user 0 com.miui.analytics #Analytics
-adb shell pm uninstall -k --user 0 com.facebook.system #Facebook App Installer
-adb shell pm uninstall -k --user 0 com.facebook.appmanager #Facebook App Manager
-adb shell pm uninstall -k --user 0 com.facebook.services #Facebook Services
-adb shell pm uninstall -k --user 0 com.android.deskclock #Reloj
-adb shell pm uninstall -k --user 0 com.google.android.youtube #Youtube
-adb shell pm uninstall -k --user 0 com.miui.msa.global #mas (main system advertising)
+    
+    adb shell pm uninstall -k --user 0 com.miui.analytics #Analytics
+    adb shell pm uninstall -k --user 0 com.facebook.system #Facebook App Installer
+    adb shell pm uninstall -k --user 0 com.facebook.appmanager #Facebook App Manager
+    adb shell pm uninstall -k --user 0 com.facebook.services #Facebook Services
+    adb shell pm uninstall -k --user 0 com.google.android.youtube #Youtube
+    adb shell pm uninstall -k --user 0 com.miui.msa.global #mas (main system advertising)
 
-echo "Finished!"
+    echo "Finished!"
+    else
+    echo "ok,exiting..."
+fi
